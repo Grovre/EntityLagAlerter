4d184803f82cded33e14282260ec1c9d311e97c9
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-13 23:28:03 -0600

    completed v1

--- src/main/java/me/grovre/lagmachinedetector/LagMachineDetector.java
+++ src/main/java/me/grovre/lagmachinedetector/LagMachineDetector.java
@@ -1,13 +1,28 @@
 package me.grovre.lagmachinedetector;
 
+import me.grovre.lagmachinedetector.listeners.OnChunkLoad;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LagMachineDetector extends JavaPlugin {
 
+    public static LagMachineDetector plugin;
+
+    public static LagMachineDetector getPlugin() {
+        return plugin;
+    }
+
+    /*
+    Permissions:
+    LagDetector.getAlert
+     */
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
+        plugin = this;
 
+        getServer().getPluginManager().registerEvents(new OnChunkLoad(), this);
     }
 
     @Override
--- src/main/java/me/grovre/lagmachinedetector/listeners/OnChunkLoad.java
+++ src/main/java/me/grovre/lagmachinedetector/listeners/OnChunkLoad.java
@@ -1,8 +1,13 @@
 package me.grovre.lagmachinedetector.listeners;
 
+import me.grovre.lagmachinedetector.LagMachineDetector;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
@@ -14,29 +19,51 @@ public class OnChunkLoad implements Listener {
 
     @EventHandler
     public void onServerLoadChunk(ChunkLoadEvent event) {
+        LagMachineDetector plugin = LagMachineDetector.getPlugin();
+        FileConfiguration config = plugin.getConfig();
+
         Chunk c = event.getChunk();
-        if (!c.isEntitiesLoaded()) {
-            System.out.println("Chunk entities not loaded... Send log to PoptartFromPluto");
-        }
-        System.out.println("Entities loaded!");
-
-        // Fills entityMap with types and how many there are
-        Map<EntityType, Integer> entityMap = new HashMap<>();
-        for(Entity e : c.getEntities()) {
-            EntityType k = e.getType(); // k is EntityType in entityMap
-            if(entityMap.containsKey(k)) {
-                entityMap.put(k, entityMap.get(k) + 1); // Sets EntityType key to previous value +1
-            } else {
-                entityMap.put(k, 1); // Sets amount of EntityType to 1
+        Bukkit.getScheduler().runTaskLater(plugin, t ->
+        {
+            if (!c.isEntitiesLoaded()) {
+                System.out.println("Checked before entities were loaded. Raise CheckDelay value in config to prevent ");
+                return;
             }
-        }
 
-        int totalEntityCount = c.getEntities().length;
-        if(totalEntityCount > 10) {
-            System.out.println("Warning! Total entity count in chunk at " + c.getX() + ", " + c.getZ() + " is greater than 10!");
-        }
+            // Fills entityMap with types and how many there are
+            Map<EntityType, Integer> entityMap = new HashMap<>();
+            for(Entity e : c.getEntities()) {
+                EntityType k = e.getType(); // k is EntityType in entityMap
+                if(entityMap.containsKey(k)) {
+                    entityMap.put(k, entityMap.get(k) + 1); // Sets EntityType key to previous value +1
+                } else {
+                    entityMap.put(k, 1); // Sets amount of EntityType to 1
+                }
+            }
 
+            int totalEntityCount = c.getEntities().length;
+            int totalEntityWarningThreshold = config.getInt("TotalEntityWarningThreshold");
+            if(totalEntityCount > totalEntityWarningThreshold) {
+                String alertMessage = "Chunk entity warning! There are " + totalEntityCount + " total entities in chunk at (chunk coords) " + c.getX() + ", " + c.getZ() + "! Visit the chunk with /lagg tpchunk [x] [z]";
+                System.out.println(alertMessage);
+                sendAlertToPermitted(alertMessage, "LagDetector.getAlert");
+            }
 
+            for(EntityType key : entityMap.keySet()) {
+                if(entityMap.get(key) > config.getInt("EntityTypeWarningThreshold")) {
+                    String alertMessage = "Chunk entity warning! There are " + entityMap.get(key) + " " + key.name() + " at (chunk coords) " + c.getX() + ", " + c.getZ() + "! Visit the chunk with /lagg tpchunk [x] [z]";
+                    System.out.println(alertMessage);
+                    sendAlertToPermitted(alertMessage, "LagDetector.getAlert");
+                }
+            }
+        }, config.getInt("CheckDelay"));
     }
 
+    public void sendAlertToPermitted(String message, String permission) {
+        for(Player player : Bukkit.getOnlinePlayers()) {
+            if(player.hasPermission(permission)) {
+                player.sendMessage(ChatColor.RED + message);
+            }
+        }
+    }
 }
