f350796740311c225b38eee40f4938827fc9ef76
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-15 11:18:54 -0600

    comments

--- src/main/java/me/grovre/lagmachinedetector/listeners/OnChunkLoad.java
+++ src/main/java/me/grovre/lagmachinedetector/listeners/OnChunkLoad.java
@@ -19,12 +19,16 @@ public class OnChunkLoad implements Listener {
 
     @EventHandler
     public void onServerLoadChunk(ChunkLoadEvent event) {
+        // Plugin and config
         LagMachineDetector plugin = LagMachineDetector.getPlugin();
         FileConfiguration config = plugin.getConfig();
 
         Chunk c = event.getChunk();
+        // Runs later to make sure the entities are loaded before running. If they're not, prints to the console
+        // that entities need to be loaded for this work and to raise the value in the config.
         Bukkit.getScheduler().runTaskLater(plugin, t ->
         {
+            // Makes sure the entities are loaded
             if (!c.isEntitiesLoaded()) {
                 System.out.println("Checked before entities were loaded. Raise CheckDelay value in config to prevent ");
                 return;
@@ -41,6 +45,7 @@ public class OnChunkLoad implements Listener {
                 }
             }
 
+            // Gets the total entity count and sends an alert if the total entity count is above the threshold
             int totalEntityCount = c.getEntities().length;
             int totalEntityWarningThreshold = config.getInt("TotalEntityWarningThreshold");
             if(totalEntityCount > totalEntityWarningThreshold) {
@@ -49,6 +54,7 @@ public class OnChunkLoad implements Listener {
                 sendAlertToPermitted(alertMessage, "LagDetector.getAlert");
             }
 
+            // Sends alert to permitted if a specific entity type's count from the map is above the threshold
             for(EntityType key : entityMap.keySet()) {
                 if(entityMap.get(key) > config.getInt("EntityTypeWarningThreshold")) {
                     String alertMessage = "Chunk entity warning! There are " + entityMap.get(key) + " " + key.name() + " at (chunk coords) " + c.getX() + ", " + c.getZ() + "! Visit the chunk with /lagg tpchunk [x] [z]";
@@ -56,6 +62,7 @@ public class OnChunkLoad implements Listener {
                     sendAlertToPermitted(alertMessage, "LagDetector.getAlert");
                 }
             }
+            // Uses the delay
         }, config.getInt("CheckDelay"));
     }
 
